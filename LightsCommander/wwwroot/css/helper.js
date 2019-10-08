1e821005982db76378b14f7786a357210319cfdc
Kam Parsen <kamparsen@yahoo.com>
2019-10-08 15:26:58 -0400

    fixed issue wirh async

--- LightsCommander/wwwroot/css/helper.js
+++ LightsCommander/wwwroot/css/helper.js
@@ -1,34 +1,38 @@
-﻿function myFunction() {
-    //document.getElementById("d1").style.fill = "yellow";
-    //setTimeout(() => { document.getElementById("d1").style.fill = "red"; document.getElementById("d2").style.fill = "yellow"; }, 1000);
-    //setTimeout(() => { document.getElementById("d2").style.fill = "red"; document.getElementById("d1").style.fill = "yellow"; }, 1000);
+﻿
+var to1;
+var to2;
+var to3;
+var to4;
+var isStop = false;
 
-   
-    //$("d1").css({ "fill": "blue" });
+function RunLeftRight() {
+
+  
     var t1 = 1000;
     var t2 = 2000;
     var t3 = 3000;
     var t4 = 4000;
 
     for (var i = 0; i < 5; i++) {
+        if (isStop) break;
 
-        setTimeout(function () {
+        to1 = setTimeout(function () {
 
             document.getElementById("d1").style.fill = "red"; document.getElementById("d2").style.fill = "white"; document.getElementById("d3").style.fill = "white"; document.getElementById("d4").style.fill = "white";
         }, t1);
 
 
-        setTimeout(function () {
+        to2 = setTimeout(function () {
 
             document.getElementById("d1").style.fill = "white"; document.getElementById("d2").style.fill = "red"; document.getElementById("d3").style.fill = "white"; document.getElementById("d4").style.fill = "white";
         }, t2);
 
-        setTimeout(function () {
+        to3 = setTimeout(function () {
 
             document.getElementById("d1").style.fill = "white"; document.getElementById("d2").style.fill = "white"; document.getElementById("d3").style.fill = "red"; document.getElementById("d4").style.fill = "white";
         }, t3);
 
-        setTimeout(function () {
+        to4 = setTimeout(function () {
 
             document.getElementById("d1").style.fill = "white"; document.getElementById("d2").style.fill = "white"; document.getElementById("d3").style.fill = "white"; document.getElementById("d4").style.fill = "red";
         }, t4); 
@@ -39,23 +43,58 @@
         t4 += 4000;
 
     }
-
- 
-      
-
-    
-
-
-   
-   
-   
- 
-    return "ss";
+    return "sucess";
 }
 
-function alertFunc() {
+function RunRightLeft() {
 
-    document.getElementById("d1").style.fill = "red";
-    setTimeout(alertFunc, 3000);
+    var t1 = 1000;
+    var t2 = 2000;
+    var t3 = 3000;
+    var t4 = 4000;
+
+   
+  
+   
+    for (var i = 0; i < 5; i++) {
+        if (isStop) break;
+        to1 = setTimeout(function () {
+
+            document.getElementById("d4").style.fill = "red"; document.getElementById("d3").style.fill = "white"; document.getElementById("d2").style.fill = "white"; document.getElementById("d1").style.fill = "white";
+        }, t1);
+
+
+        to2 = setTimeout(function () {
+
+            document.getElementById("d4").style.fill = "white"; document.getElementById("d3").style.fill = "red"; document.getElementById("d2").style.fill = "white"; document.getElementById("d1").style.fill = "white";
+        }, t2);
+
+        to3 = setTimeout(function () {
+
+            document.getElementById("d4").style.fill = "white"; document.getElementById("d3").style.fill = "white"; document.getElementById("d2").style.fill = "red"; document.getElementById("d1").style.fill = "white";
+        }, t3);
+
+        to4 = setTimeout(function () {
+
+            document.getElementById("d4").style.fill = "white"; document.getElementById("d3").style.fill = "white"; document.getElementById("d2").style.fill = "white"; document.getElementById("d1").style.fill = "red";
+        }, t4);
+
+        t1 += 4000;
+        t2 += 4000;
+        t3 += 4000;
+        t4 += 4000;
+
+    }
+    return "sucess";
+}
+
+function dothis() {
+    isStop = true;
+    clearTimeout(to1);
+    clearTimeout(to2);
+    clearTimeout(to3);
+    clearTimeout(to4);
+
+    return "ss";
 }
 
